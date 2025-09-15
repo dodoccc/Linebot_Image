50586a3237868847a6b98f954540cc74ea0e98bf
dodoccc <122043866+dodoccc@users.noreply.github.com>
2025-09-15 18:07:07 +0800

    Update sketch.py

--- sketch.py
+++ sketch.py
@@ -2,13 +2,13 @@ from PIL import Image #導入PIL
 from tqdm import tqdm
 
 def sketch(imgmsg):
-    img = Image.open(f'{imgmsg}') #打開图片
-    new = Image.new("L", img.size, 255) #新文件，大小和原来文件一样
-    width, height = img.size #图片大小
+    img = Image.open(f'{imgmsg}') #打開圖片
+    new = Image.new("L", img.size, 255) #新文件，大小同原文件
+    width, height = img.size #圖片大小
     img = img.convert("L")
 
-    Pen_size = 3 #int(input('Pensize:')) #画笔大小
-    Color_diff = 3 #int(input('Color diffusion variable:')) #色差扩散器
+    Pen_size = 3 #int(input('Pensize:')) #畫筆大小
+    Color_diff = 3 #int(input('Color diffusion variable:')) #色差
 
     for i in tqdm(range(Pen_size + 1, width - Pen_size - 1)): 
         for j in range(Pen_size + 1, height - Pen_size - 1):
@@ -40,4 +40,5 @@ def sketch(imgmsg):
             if abs(qcolor - wcolor) > Color_diff:
                 originalColor -= (255 - img.getpixel((i, j))) // 4
                 new.putpixel((i, j), originalColor)
+
     new.save(f'sketch7_{imgmsg}')
